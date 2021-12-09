42b8b3257f95af10db4ede1720d71f350d12ad37
Min <qwer810520@gmail.com>
2021-12-10 00:30:07 +0800

    git init

---
+++ CoordinatorDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CoordinatorDemo
+//
+//  Created by Min on 2021/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view.backgroundColor = .systemPink
+    }
+}
+
